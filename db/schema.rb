3d5abffb5de87891cb4118e850f52c1f6b61b38a
ryotafff <jnblog2019@gmail.com>
2020-08-30 04:20:34 +0000

    fix table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_105239) do
+ActiveRecord::Schema.define(version: 2020_07_21_062550) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
